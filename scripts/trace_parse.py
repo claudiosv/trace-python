ddfbc92f2a1698b4bb5a469e2443d30ae30c496d
Claudio Spiess <claudiosv@users.noreply.github.com>
2022-04-06 14:51:14 -0700

    Configurable source retrieval

--- scripts/trace_parse.py
+++ scripts/trace_parse.py
@@ -31,7 +31,7 @@ def clean_event(event):
     return event
 
 
-def get_core_methods(path, index_traces=True):
+def get_core_methods(path, test_path, index_traces=True):
     """Generates a stream of core method events from a traced test suite."""
     # Use an expanding set of all method calls starting from a core/test method to track where we are in the call tree.
     # This set is initialized & updated with expected method events as soon as we enter a test case and should be empty
@@ -39,8 +39,15 @@ def get_core_methods(path, index_traces=True):
     fanout = set()
     line_ix = 0
     suite_name = path.name.replace("_", "/").split("/")[:-1]
+    if test_path:
+        search_root = test_path
+    else:
+        search_root = (path.parents[1]
+            / "src"
+            / "test"
+            / "java")
     heuristic_path = (
-        path.parents[1] / "src" / "test" / "java" / ("/".join(suite_name) + ".java")
+        search_root / ("/".join(suite_name) + ".java")
     )
 
     size = 0
@@ -203,14 +210,17 @@ def count_java_calls(call_counter: Counter) -> int:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process a gzipped trace.")
+    parser.add_argument("--source_path", metavar="Path", type=Path, required=False)
+    parser.add_argument("--test_path", metavar="Path", type=Path, required=False)
+    parser.add_argument("--show_source", type=bool, action=argparse.BooleanOptionalAction)
+    parser.add_argument("--verbose", type=bool, action=argparse.BooleanOptionalAction)
     parser.add_argument("path", metavar="Path", type=Path)
-    parser.add_argument("show_source", type=bool)
-    parser.add_argument("verbose", type=bool)
-    parser.add_argument("mode", choices=["one_call", "extract", "loc"])
+
+    # parser.add_argument("mode", choices=["one_call", "extract", "loc"])
     args = parser.parse_args()
-    one_call = args.mode == "one_call"
-    loc = args.mode == "loc"
-    extract = args.mode == "extract"
+    # one_call = args.mode == "one_call"
+    # loc = args.mode == "loc"
+    # extract = args.mode == "extract"
 
     dumps = {}
     dump_counter = {}
@@ -218,6 +228,7 @@ if __name__ == "__main__":
     file_stem = file_name.stem
     if os.path.getsize(file_name) < 1:
         exit()
+    # source_path = Path()
     files = {}
     string_traces = []
     visited = []
@@ -226,7 +237,7 @@ if __name__ == "__main__":
 
     method_dicts = []
 
-    for method in get_core_methods(file_name):
+    for method in get_core_methods(file_name, args.test_path):
         call_counter = Counter()
         java_calls = ""
         all_calls = ""
@@ -247,11 +258,15 @@ if __name__ == "__main__":
             )
             print(f"        Method makes {java_call_count} calls to java:{all_calls}")
 
-        heuristic_path = (
-            file_name.parents[1]
+        if args.source_path:
+            search_root = args.source_path
+        else:
+            search_root = (file_name.parents[1]
             / "src"
             / "main"
-            / "java"
+            / "java")
+        heuristic_path = (
+            search_root
             / (class_name.replace(".", "/").partition("$")[0] + ".java")
         )
         size = 0
